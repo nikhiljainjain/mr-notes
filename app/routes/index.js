3fdc4916de6284e6cdbdfd343f82e7df48c1b128
NIKHIL JAIN <34137170+nikhiljainjain@users.noreply.github.com>
2020-03-21 20:38:47 +0530

    landing page is ready

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,11 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
+	res.render('index');
 });
 
+
+router.get('/login-signup', (req, res, next)=>{
+	res.render('login-signup');
+});
 module.exports = router;
