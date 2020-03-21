f7e41d33ecde11fbaf23c59daf60c27e71d5a656
NIKHIL JAIN <34137170+nikhiljainjain@users.noreply.github.com>
2020-03-21 22:11:41 +0530

    login & signup page ready

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,28 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('index');
+	res.render('index', { lgbtn: true });
 });
 
 
 router.get('/login-signup', (req, res, next)=>{
-	res.render('login-signup');
+	res.render('login-signup', { lgbtn: false });
 });
+
+router.post('/login', (req, res, next)=>{
+	console.log(req.body);
+	res.status(302).redirect('/dashboard');
+	//res.render('dash')
+});
+
+router.post('/signup', (req, res, next)=>{
+	console.log(req.body);
+	res.status(302).redirect('/dashboard');
+	//res.render('dashboard', { lgbtn: false });
+});
+	
+router.get('/dashboard', (req, res, next)=>{
+	res.send('<h1>Available soon</h1>');
+});
+
 module.exports = router;
