8b60c8a1f9655fbbcfbaf21a6c023b85c907c805
NIKHIL JAIN <nikhiljain.ashajain@gmail.com>
2020-06-06 16:37:20 +0530

    fixed(gender input fixed)

--- app/function/cookies.js
+++ app/function/cookies.js
@@ -6,7 +6,7 @@ let { invalidRes, COOKIES_AGE } = require('../config');
 
 //user cookies validation
 const cookieValid = (req, res, next) =>{
-	//console.log("COOKIE PART")
+	console.log("COOKIE PART", req.cookies);
 	//extracting cookies from req parameter
   	let cookie = req.cookies.token;
 	if (cookie != null){
@@ -14,10 +14,10 @@ const cookieValid = (req, res, next) =>{
 			//token validation from jwt
 			cookie = jwt.verify(cookie, process.env.JWT_SECRET);
 			cookie = cookie.token;
-			//console.log(cookie)
+			console.log("COOKIE", cookie);
 			/*.populate("notes")*/
 			User.findOne({cookie}).exec((err, data)=>{
-				console.log(data);
+				console.log("UserData=", data);
 				if (err) console.error.bind(err);
 				if (data){
 					req.data = data;
--- app/routes/index.js
+++ app/routes/index.js
@@ -10,10 +10,7 @@ const { bodyDataValidCred, bodyDataValidJSON } = require('../function');
 const { jwtCreate } = require("../function/cookies"); 
 
 //home page
-router.get('/', (req, res)=>{
-	console.log(req.statusCode)
-	res.render('index');
-});
+router.get('/', (req, res)=>res.render('index'));
 
 //rendering login & signup page
 router.get('/login-signup', (req, res)=>{
@@ -33,18 +30,18 @@ router.get('/login-signup', (req, res)=>{
 	}
 });
 
-router.get('/login', (req, res)=>{
-	res.status(302).redirect("/login-signup");
-});
+router.get('/login', (req, res)=>res.status(302).redirect("/login-signup"));
+
+router.get('/signup', (req, res)=>res.status(302).redirect("/login-signup"));
+
 
 //user login
 router.post('/login', bodyDataValidCred, jwtCreate, (req, res)=>{
-
 	//creating session variable to limit the login attempt
 	if (req.cookies.count || (req.session.loginCount && req.session.loginCount > 5)){
 		ERROR_MSG = "Try After 24hours";
 		//restricting user for 24 hour to login attempt
-		res.cookie('count', "dont", { maxAge: (COOKIES_AGE/400) }).status(302).redirect(`/login-signup?q=${ERROR_MSG}`);
+		return res.cookie('count', "dont", { maxAge: (COOKIES_AGE/400) }).status(302).redirect(`/login-signup?q=${ERROR_MSG}`);
 	}else{
 		ERROR_MSG = 'Invalid credentials';
 	
@@ -62,7 +59,7 @@ router.post('/login', bodyDataValidCred, jwtCreate, (req, res)=>{
 		//finding user in db
 		User.findOne({ email }, "password", (err, user)=>{
 			if (err) console.error.bind('Database Error', err);
-			console.log("User=", user);
+			//console.log("User=", user);
 			if (user && bcrypt.compareSync(password, user.password)){
 				//setting cookies in db
 				user.set({ cookie: req.data.token });
@@ -72,71 +69,59 @@ router.post('/login', bodyDataValidCred, jwtCreate, (req, res)=>{
 						//console.log("req data", req.data);
 						//sending response
 						if (err) console.error.bind("Session error", err);
-						res.cookie('token', req.data.jwt, COOKIE_PROP).status(302).redirect('/users');
+						return res.cookie('token', req.data.jwt, COOKIE_PROP).status(302).redirect('/users');
 					});
 				});
 			}else{
 				req.session.loginCount++;
-				res.status(302).redirect(`/login-signup?q=${ERROR_MSG}`);
+				return res.status(302).redirect(`/login-signup?q=${ERROR_MSG}`);
 			}
 		});
 	}
 });
 
-
-router.get('/signup', (req, res)=>res.status(302).redirect("/login-signup"));
-
 //user registration
-router.post('/signup', bodyDataValidCred, jwtCreate, (req, res)=>{
+router.post('/signup', bodyDataValidCred, jwtCreate, async (req, res)=>{
 	ERROR_MSG = "Password and Confirm Password are not same";
-	
+	//console.log(req.body, req.ip);
 	if (req.body.password == req.body.cpassword){
 
-		//checking for existence of user		
-		User.findOne({ email: req.body.email }, (err, userExist)=>{
-			//console.log(userExist);
+		try{
+			delete req.body.cpassword
 
-			if (err) console.error.bind("DB error", err);
-			//if user exist then sending back to login page
-			if (userExist){
+			let newUser = {
+				...req.body,
+				cookie: req.data.token,
+				ipAddress: req.ip, 
+				verificationCode: null
+			};
+			//hashing password
+			newUser.password = bcrypt.hashSync(req.body.password);
+			//creating new user for the data
+			newUser = new User(newUser);
+
+			//console.log(newUser, req.data);
+
+			//sending email to user and email verification process will start
+			await newUser.save();
+			return res.cookie('token', req.data.jwt, COOKIE_PROP).status(302).redirect('/users');
+			
+		}catch(err){
+			console.log("ERROR", err);
+			if(err.code == 11000){
 				ERROR_MSG = "User Already Exist";
-				res.status(302).redirect(`/login-signup?q=${ERROR_MSG}&color=green`);
+				return res.redirect(`/login-signup?q=${ERROR_MSG}&color=green`);
 			}else{
-				delete req.body.cpassword
-
-				let newUser = {
-					...req.body,
-					cookie: req.data.token,
-					registerIP: req.ip, 
-					verificationCode: null
-				};
-				//hashing password
-				newUser.password = bcrypt.hashSync(req.body.password);
-				//creating new user for the data
-				newUser = new User(newUser);
-
-				//console.log(newUser);
-
-				//sending email to user and email verification process will start
-				newUser.save().then(()=>{
-					req.session.regenerate((err)=>{
-						if (err) console.error.bind("Session error", err);
-						//console.log(req.data);
-						//setting cookies
-						res.cookie('token', req.data.jwt, COOKIE_PROP).status(302).redirect('/users');
-					});
-				});
+				ERROR_MSG = "SOMETHING HAPPENING WRONG";
+				return res.redirect(`/login-signup?q=${ERROR_MSG}`);
 			}
-		});
+		}
 	}else
-		res.status(302).redirect(`/login-signup?q=${ERROR_MSG}`);
+		return res.status(302).redirect(`/login-signup?q=${ERROR_MSG}`);
 });
 
 //forget password page
-router.get('/forget-password', (req, res)=>{
-	//need to implement
-	res.render("forget-password", ejsData);
-});
+router.get('/forget-password', (req, res)=>res.render("forget-password", ejsData));
 
 router.post('/forget-password', bodyDataValidJSON, (req, res)=>{
 	//prcoess forget password
@@ -148,9 +133,7 @@ router.post('/forget-password', bodyDataValidJSON, (req, res)=>{
 	res.json(validRes);
 });
 
-router.get('/forget-password/code/:verificationCode', (req, res)=>{
-	res.render('new-password', ejsData);
-});
+router.get('/forget-password/code/:verificationCode', (req, res)=>res.render('new-password', ejsData));
 
 router.post('/forget-password/code/:verificationCode', bodyDataValidJSON, (req, res)=>{
 	res.json(validRes);
